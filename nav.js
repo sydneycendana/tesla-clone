eb87240dd61222748292f3c08c3ec756e5331ad4
Sydney Cendana <sydneycendana@gmail.com>
2022-11-30 13:46:31 -0800

    sidebar menu opens

--- nav.js
+++ nav.js
@@ -1,14 +1,15 @@
 const openMenu = () => {
-    document.querySelector('.backdrop').className = 'backdrop active'
+    document.querySelector('.backdrop').className = 'backdrop active';
     document.querySelector('aside').className = 'active';
 };
 
 const closeMenu = () => {
-    document.querySelector('.backdrop').className = 'backdrop'
+    document.querySelector('.backdrop').className = 'backdrop';
     document.querySelector('aside').className = '';
 };
 
 
 document.getElementById('menuBtn').onclick = e => {
     e.preventDefault();
+    openMenu();
 }
