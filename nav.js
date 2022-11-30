5a733e652873ac842d436e24fce8fde74a2af8ac
Sydney Cendana <sydneycendana@gmail.com>
2022-11-29 19:00:06 -0800

    nav.js

---
+++ nav.js
@@ -0,0 +1,14 @@
+const openMenu = () => {
+    document.querySelector('.backdrop').className = 'backdrop active'
+    document.querySelector('aside').className = 'active';
+};
+
+const closeMenu = () => {
+    document.querySelector('.backdrop').className = 'backdrop'
+    document.querySelector('aside').className = '';
+};
+
+
+document.getElementById('menuBtn').onclick = e => {
+    e.preventDefault();
+}
